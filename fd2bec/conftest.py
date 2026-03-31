2ad3d858bdd20199f6f02a5e3dd743d5e5465a15
Elia Stocco <elia.stocco@mpsd.mpg.de>
2026-03-31 15:29:20 +0200

    fixed?

--- fd2bec/conftest.py
+++ fd2bec/conftest.py
@@ -2,5 +2,5 @@ import os
 from pathlib import Path
 
 test_dir = Path(__file__).parent.parent/"tests"
-repo_root = Path(os.path.dirname(os.path.dirname(__file__)))
+repo_root = Path(os.path.dirname(__file__))
 structures_dir = repo_root / "structures"
