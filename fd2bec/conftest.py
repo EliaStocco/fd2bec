f09f662022d603caa0fd35b76bad7cb845be914b
Elia Stocco <elia.stocco@mpsd.mpg.de>
2026-03-31 15:01:19 +0200

    updated

--- fd2bec/conftest.py
+++ fd2bec/conftest.py
@@ -2,8 +2,5 @@ import os
 from pathlib import Path
 
 test_dir = Path(__file__).parent.parent/"tests"
-
-# Get the root of the repo relative to this test file
 repo_root = Path(os.path.dirname(os.path.dirname(__file__)))
-
 structures_dir = repo_root / "structures"
