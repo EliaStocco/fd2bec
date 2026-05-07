a9b04ace44a48fcbe2fe99bc61ae18ebe44910c4
Elia Stocco <elia.stocco@mpsd.mpg.de>
2026-05-07 11:23:34 +0200

    cleaning

--- fd2bec/atomic.py
+++ fd2bec/atomic.py
@@ -340,42 +340,7 @@ class AtomicStructure:
         cell, frac_pos, numbers = self.standardized
         return type(self)(
             cell=Cell(cell), frac_pos=frac_pos, symbols=numbers2symbols(numbers), check=False
-        )
-
-    # @validate_types
-    def get_symmetry_operations(self, basis: Basis = "cartesian") -> Tuple[np.ndarray, np.ndarray]:
-        """Return space/point group symmetry operatios (R, t) such that x' = R x + t."""
-
-        if basis == "fractional":
-            assert self.pbc, "'fractional' is supported only for periodic structures."
-            return self._spglib_dataset.rotations, self._spglib_dataset.translations
-
-        elif basis == "cartesian":
-            if self.pbc:
-                R = Rotation(
-                    data=self._spglib_dataset.rotations, basis="fractional", cell=self.cell
-                ).to("cartesian")
-                T = Translation(
-                    data=self._spglib_dataset.translations, basis="fractional", cell=self.cell
-                ).to("cartesian")
-
-                return R.data, T.data
-            else:
-                pga = PointGroupAnalyzer(
-                    self.__pymatge_molecule,
-                    tolerance=self.symprec,
-                    eigen_tolerance=self.symprec,
-                    matrix_tolerance=self.symprec,
-                )
-
-                S = pga.get_symmetry_operations()
-                R = np.asarray([s.rotation_matrix for s in S])
-                T = np.asarray([s.translation_vector for s in S])
-
-                return R, T
-
-        else:
-            raise NotImplementedError
+        )    
 
     def _test_symmetry(self, basis: Basis = "cartesian", atol=ATOL):
         """
@@ -454,54 +419,6 @@ class AtomicStructure:
             new_structure = self.clone(positions=new_pos)
             mapping[n] = self._get_atoms_mapping(new_structure)
         return np.asarray(mapping)
-
-    # def __get_all_atoms_mapping(self, debug=DEBUG):
-    #     """
-    #     Compute inverse atom index mappings for all symmetry operations.
-
-    #     For each space-group operation (R, t), the function applies the transformation
-    #     to the fractional coordinates, builds the transformed structure, and determines
-    #     how atom indices map back to the original structure.
-
-    #     Returns an array inv_map such that for each operation k:
-    #         inv_map[k, i] gives the index in the transformed structure corresponding to
-    #         atom i in the original structure.
-
-    #     Parameters
-    #     ----------
-    #     debug : bool, optional
-    #         If True, performs consistency checks on the mappings.
-    #     **kwargs :
-    #         Passed to the spglib cell construction.
-
-    #     Returns
-    #     -------
-    #     inv_map : ndarray of shape (Nops, Natoms)
-    #         Inverse atom mappings for each symmetry operation.
-    #     """
-    #     spg = self._spglib_dataset
-    #     R = spg.rotations
-    #     T = spg.translations
-    #     mappings = [None] * len(R)
-    #     for n, (r, t) in enumerate(zip(R, T)):
-    #         new_pos = self.frac_pos @ r.T + t
-    #         new_structure = self.clone(frac_pos=new_pos)
-    #         mappings[n] = self._get_atoms_mapping(new_structure)
-    #     mappings = np.asarray(mappings)
-    #     inv_map = invert_indices(mappings, axis=1)
-
-    #     if debug:
-    #         for r, t, m, im in zip(R, T, mappings, inv_map):
-    #             new_pos = self.frac_pos @ r + t
-    #             if not np.allclose(wrap(new_pos - self.frac_pos[m]), 0, atol=SYMPREC):
-    #                 raise ValueError("Error in computing atom mapping for symmetry operation.")
-    #             if not np.allclose(wrap(new_pos[im] - self.frac_pos), 0, atol=SYMPREC):
-    #                 raise ValueError("Error in computing atom mapping for symmetry operation.")
-    #             new_pos = self.frac_pos[im] @ r + t
-    #             if not np.allclose(wrap(new_pos - self.frac_pos), 0, atol=SYMPREC):
-    #                 raise ValueError("Error in computing atom mapping for symmetry operation.")
-
-    #     return inv_map
 
     def get_tensor_symmetry_operations(self, tensor: Tensor):
         """
@@ -610,6 +527,40 @@ class AtomicStructure:
         H = affine2homogeneous(R_flat, T_flat)
         return H
 
+    def get_symmetry_operations(self, basis: Basis = "cartesian") -> Tuple[np.ndarray, np.ndarray]:
+        """Return space/point group symmetry operatios (R, t) such that x' = R x + t."""
+
+        if basis == "fractional":
+            assert self.pbc, "'fractional' is supported only for periodic structures."
+            return self._spglib_dataset.rotations, self._spglib_dataset.translations
+
+        elif basis == "cartesian":
+            if self.pbc:
+                R = Rotation(
+                    data=self._spglib_dataset.rotations, basis="fractional", cell=self.cell
+                ).to("cartesian")
+                T = Translation(
+                    data=self._spglib_dataset.translations, basis="fractional", cell=self.cell
+                ).to("cartesian")
+
+                return R.data, T.data
+            else:
+                pga = PointGroupAnalyzer(
+                    self.__pymatge_molecule,
+                    tolerance=self.symprec,
+                    eigen_tolerance=self.symprec,
+                    matrix_tolerance=self.symprec,
+                )
+
+                S = pga.get_symmetry_operations()
+                R = np.asarray([s.rotation_matrix for s in S])
+                T = np.asarray([s.translation_vector for s in S])
+
+                return R, T
+
+        else:
+            raise NotImplementedError
+        
     def get_totally_symmetric_projection(self, tensor: Tensor):
         """Construct the projection operator onto the totally symmetric representation."""
         G, T = self.get_tensor_symmetry_operations(tensor=tensor)
